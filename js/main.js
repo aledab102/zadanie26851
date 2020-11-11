395f0945c8f39e43eb3cfc67ba257889f083b8d4
A.Dabrowska <aledabrowska@gmail.com>
2020-11-11 11:55:34 +0100

    start

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = '2';
+let secondNumber = '5';
+
+function sum(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber
+    
+    return result;
+}
+
+
+console.log( sum(2, 5) );
